009042f51ddb9fd147c1f8aeec53f5fdcfa924e1
Struchkov Mark <mark@struchkov.dev>
2024-10-11 00:42:10 +0300

    update

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run --name liquibase --network host -v YOUR_PATH_TO_db/changelog:/liquibase/db/changelog liquibase/liquibase:4.9.1 --defaultsFile=/liquibase/db/changelog/liquibase.properties update
+docker run --name liquibase --network host -v ./liquibase-4.29.2/db/changelog:/liquibase/db/changelog liquibase/liquibase:4.29.2 --defaultsFile=/liquibase/db/changelog/liquibase.properties update
 docker rm liquibase
